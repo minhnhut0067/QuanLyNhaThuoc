bf0b67330c42c6522278377a82a5faded8f124af
minhnhut0067 <minhnhut0056@gmail.com>
2016-11-02 08:53:02 +0700

    Start

---
+++ Data_Sql/MS_CREATE_DATABASE.sql
@@ -0,0 +1,15 @@
+﻿-- Role: msolution
+
+-- DROP ROLE msolution; Pass msolution
+CREATE ROLE msolution LOGIN
+  ENCRYPTED PASSWORD 'md56785a8e9dbbe647823d555690133ed3a'
+  SUPERUSER INHERIT CREATEDB CREATEROLE REPLICATION;
+  
+-- Database: ms_medical
+-- DROP DATABASE ms_medical;
+CREATE DATABASE ms_root WITH OWNER = msolution ENCODING = 'UTF8'
+TABLESPACE = pg_default LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252'
+CONNECTION LIMIT = -1;
+
+CREATE public database link meta connect to msolution identified by 'msolution' using libpq 'host=localhost port=5435 dbname=cm_meta';
+drop public database link meta;
